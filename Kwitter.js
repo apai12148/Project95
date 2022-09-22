b932699910b7615487b0f23cb40814dbc91b521b
apai12148 <97473664+apai12148@users.noreply.github.com>
2022-09-22 19:38:18 -0400

    Add files via upload

--- Kwitter.js
+++ Kwitter.js
@@ -1,5 +1,5 @@
 function addUser(){
-    user_name = document.getElementById("user_name_input").value;
+    user_name = document.getElementById("user_name").value;
 
     localStorage.setItem("user_name", user_name);
 
